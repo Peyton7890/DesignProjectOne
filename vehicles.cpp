5bf323a342eb593cc39e0c2e8d4d3a9e2931adb0
Peyton7890 <peytonmartin7890@gmail.com>
2020-09-21 21:51:58 -0400

    added vehicles and people classes

---
+++ vehicles.cpp
@@ -0,0 +1,7 @@
+/**********************************************************/
+/*** Title: main.cpp                        						***/
+/*** Course: Computational Problem Solving CPET-II	   	***/
+/*** Developers:	Peyton Martin and Eric Guarracina			***/
+/*** Date : 09/21/2020                  								***/
+/*** Description: vehicles class                       	***/
+/**********************************************************/
