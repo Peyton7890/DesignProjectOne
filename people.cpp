4fa2cfd356c015d88f7f209566a72b993574d501
Peyton7890 <peytonmartin7890@gmail.com>
2020-09-23 11:05:17 -0400

    Fixed all the header spacings

--- people.cpp
+++ people.cpp
@@ -1,7 +1,7 @@
 /**********************************************************/
-/*** Title: people.cpp                       						***/
-/*** Course: Computational Problem Solving CPET-II	   	***/
-/*** Developers:	Peyton Martin and Eric Guarracina			***/
-/*** Date : 09/21/2020                  								***/
-/*** Description: people class                        	***/
+/*** Title: people.cpp                                  ***/
+/*** Course: Computational Problem Solving CPET-II      ***/
+/*** Developers:  Peyton Martin and Eric Guarracina     ***/
+/*** Date : 09/21/2020                                  ***/
+/*** Description: people class                          ***/
 /**********************************************************/
