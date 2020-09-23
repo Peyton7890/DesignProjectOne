8b8f417117639ee4a899feded43724b7be02c755
Peyton7890 <peytonmartin7890@gmail.com>
2020-09-23 11:09:08 -0400

    fixed main and people.h

--- people.h
+++ people.h
@@ -1,7 +1,7 @@
 /**********************************************************/
 /*** Title: people.h                                    ***/
 /*** Course: Computational Problem Solving CPET-II      ***/
-/*** Developers:	Peyton Martin and Eric Guarracina     ***/
+/*** Developers:  Peyton Martin and Eric Guarracina     ***/
 /*** Date : 09/21/2020                                  ***/
 /*** Description: header file for people class          ***/
 /**********************************************************/
