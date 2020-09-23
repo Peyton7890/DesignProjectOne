ecf708dc89aac225ba7dc8b2bad93180d5143a99
Peyton7890 <peytonmartin7890@gmail.com>
2020-09-23 11:01:59 -0400

    Actually fixed vehicles.h header file

--- vehicles.h
+++ vehicles.h
@@ -1,7 +1,7 @@
 /**********************************************************/
 /*** Title: vehicles.h                                  ***/
 /*** Course: Computational Problem Solving CPET-II      ***/
-/*** Developers:	Peyton Martin and Eric Guarracina     ***/
+/*** Developers:  Peyton Martin and Eric Guarracina     ***/
 /*** Date : 09/21/2020                                  ***/
 /*** Description: header file for vehicles class        ***/
 /**********************************************************/
