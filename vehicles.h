2d7079feed6c3815c02d10253761e53c5982588c
Peyton7890 <peytonmartin7890@gmail.com>
2020-09-25 10:33:50 -0400

    started vehicles header

--- vehicles.h
+++ vehicles.h
@@ -5,3 +5,23 @@
 /*** Date : 09/21/2020                                  ***/
 /*** Description: header file for vehicles class        ***/
 /**********************************************************/
+#include iostream
+
+using namespace std;
+
+class Vehicles {
+  private:
+    string vehicleColor;
+    int availableSeats;
+    vector<bool> seatOccupancy 18;
+
+    int frontSeatVal = 1;
+    int backSeatVal = 3;
+    int sideBackSeatBal = 2;
+    int middleBackSeatVal = 1;
+
+
+  public:
+    void displayVehicle();
+    
+}
