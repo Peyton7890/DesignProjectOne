102d97bbcb1caaf825b38495f477fbe919a13eb4
Peyton7890 <peytonmartin7890@gmail.com>
2020-09-25 10:25:45 -0400

    started people header

--- people.h
+++ people.h
@@ -5,3 +5,19 @@
 /*** Date : 09/21/2020                                  ***/
 /*** Description: header file for people class          ***/
 /**********************************************************/
+#include iostream
+
+using namespace std;
+
+class People {
+  private:
+    int numOfCredits = 20;
+    string name;
+  public:
+    void assignPassenger(string);
+    void printPassenger();
+
+    void assignDriver(string);
+    void printDriver();
+
+}
