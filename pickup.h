f81b9330a8c1682c412573c60781b950cbeddf2c
Peyton7890 <peytonmartin7890@gmail.com>
2020-09-25 09:35:13 -0400

    added vehicles sub classes

---
+++ pickup.h
@@ -0,0 +1,7 @@
+/**********************************************************/
+/*** Title: people.h                                    ***/
+/*** Course: Computational Problem Solving CPET-II      ***/
+/*** Developers:  Peyton Martin and Eric Guarracina     ***/
+/*** Date : 09/21/2020                                  ***/
+/*** Description: header file for pickup class          ***/
+/**********************************************************/
