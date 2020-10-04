f1022d7816570a6aac98bc2e4551aef1e50570ca
Peyton7890 <peytonmartin7890@gmail.com>
2020-10-04 13:09:14 -0400

    Finished Main

--- main.cpp
+++ main.cpp
@@ -33,20 +33,20 @@ int main() {
 
 //Inputting all of the drivers into a vector
     for (int i = 0; i < 6; i++) {
-        string temp;
-        getline(inputFile, temp);
-        driverNames.push_back(temp);
+      string temp;
+      getline(inputFile, temp);
+      driverNames.push_back(temp);
     }
 
 //Inputting all of the players and their credits into vectors
     for (int i = 0; i < 18; i++) {
-        string name, first, last;
-        int credit;
+      string name, first, last;
+      int credit;
 
-        inputFile >> first >> last >> credit;
-        name = first + " " + last;
-        playerNames.push_back(name);
-        playerCredits.push_back(credit);
+      inputFile >> first >> last >> credit;
+      name = first + " " + last;
+      playerNames.push_back(name);
+      playerCredits.push_back(credit);
     }
 
     inputFile.close();
@@ -56,37 +56,36 @@ int main() {
 
     //Sets all drivers as part of the people class
     for(int i = 0; i < drivers.size(); i++){
-        drivers.at(i).assignDriver(driverNames.at(i));
+      drivers.at(i).assignDriver(driverNames.at(i));
     }
 
     //Sets all passengers as part of the people class
     for(int i = 0; i < passengers.size(); i++){
-        passengers.at(i).assignPassenger(playerNames.at(i), playerCredits.at(i));
+      passengers.at(i).assignPassenger(playerNames.at(i), playerCredits.at(i));
     }
 
-    cout << "Enter 'q' to quit, 'c' to create reservation, 'm' to modify reservation, and 'd' to delete" << endl;
+    cout << "Enter 'q' to quit, 'c' to create reservation, 'm' to modify";
+    cout << " reservation, and 'd' to delete" << endl;
     cin >> userInput;
 
     while (userInput != 'q') {
       if (userInput == 'c') {
         cout << "Enter Player to Create" << endl;
         drivers.at(1).createReservation(drivers.at(1));
-        cout << "Enter 'q' to quit, 'c' to create reservation, 'm' to modify reservation, and 'd' to delete" << endl;
       }
       else if (userInput == 'm') {
         cout << "Enter Player to Modify" << endl;
         drivers.at(1).modifyReservation(drivers.at(1));
-        cout << "Enter 'q' to quit, 'c' to create reservation, 'm' to modify reservation, and 'd' to delete" << endl;
       }
       else if (userInput == 'd') {
         cout << "Enter Player to Delete" << endl;
         drivers.at(1).deleteReservation(drivers.at(1));
-        cout << "Enter 'q' to quit, 'c' to create reservation, 'm' to modify reservation, and 'd' to delete" << endl;
       }
       else {
         cout << "Not a valid option, please try again." << endl;
-        cout << "Enter 'q' to quit, 'c' to create reservation, 'm' to modify reservation, and 'd' to delete" << endl;
       }
+      cout << "Enter 'q' to quit, 'c' to create reservation, 'm' to modify";
+      cout << " reservation, and 'd' to delete" << endl;
       cin >> userInput;
     }
   return 0;
